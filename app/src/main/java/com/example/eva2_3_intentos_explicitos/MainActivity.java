8d882bb40a000e8c62f23adc314b10e8d7723e5d
IsraelMaganaSolis <israelmaganasolis@gmail.com>
2020-12-11 20:26:20 -0700

    Initial commit

---
+++ app/src/main/java/com/example/eva2_3_intentos_explicitos/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.eva2_3_intentos_explicitos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Action: Objeto que lanza la nueva actividad
+        //Data: La clase de la nueva actividad
+        intent = new Intent(this,SecundaryActivity.class);
+    }
+    public void onClick(View v){
+        startActivity(intent);
+    }
+}
